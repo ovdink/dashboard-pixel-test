f64b32b18adfbdc6d8be72b27b68332307a836c8
henker <ovdink@gmail.com>
2024-04-19 13:55:01 +0300

    added fonts

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,5 +1,13 @@
 import * as STYLE from '../units';
 
 export const MainPage = () => {
-    return <STYLE.PageWrapper>TEST</STYLE.PageWrapper>;
+    return (
+        <STYLE.PageWrapper>
+            {/* test font */}
+            <h1 style={{ color: 'white' }}>Invite friends and earn more points</h1>
+            <h1 style={{ fontFamily: 'PixelMix', color: 'white' }}>
+                Invite friends and earn more points
+            </h1>
+        </STYLE.PageWrapper>
+    );
 };
